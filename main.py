0ce133c396a4dd486617f4f271255678700c215f
ablz0214 <138658346+ablz0214@users.noreply.github.com>
2025-03-18 16:50:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,30 +7,26 @@ import re
 from . import util
 
 # 注册插件
-@register(name="RemoveTagsAndSegment", description="适用于DeepSeekR1的去标签及分段插件", version="0.1", author="ablz")
+@register(name="DeepSeekR1nothink--update", description="remove think info for DeepSeekR1 model", version="0.1", author="abu")
 class URLMaskerPlugin(Plugin):
 
     # 插件加载时触发
+    # plugin_host (pkg.plugin.host.PluginHost) 提供了与主程序交互的一些方法，详细请查看其源码
     def __init__(self, plugin_host: PluginHost):
         pass
-
+        
     @handler(PromptPreProcessing)
     async def _(self, ctx: EventContext):
         if len(ctx.event.prompt) != 0:
-            for promptindex, promptcontent in enumerate(ctx.event.prompt):
+            for promptindex,promptcontent in enumerate(ctx.event.prompt):
                 if promptindex % 2 != 0:
                     ctx.event.prompt[promptindex].content = re.sub(r'<think>[\s\S]*?<\/think>\n\n', '', promptcontent.content)
 
     @on(NormalMessageResponded)
-    def group_normal_message_received(self, event: EventContext, ​**kwargs):
+    def group_normal_message_received(self, event: EventContext, **kwargs):
         msg = kwargs['response_text']
         ret = util.removethink(msg)
-
-        # 将 ret 分段，每段最长 1024 个字节
-        ret_segments = [ret[i:i + 1024] for i in range(0, len(ret), 1024)]
-
-        # 添加分段后的回复
-        event.add_return('reply', ret_segments)
+        event.add_return('reply', ret)
 
     # 插件卸载时触发
     def __del__(self):
